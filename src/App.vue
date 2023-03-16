8a7910f6925e123b90b2e3ce7edc1e0188b2362e
Chad Misiuk <misiuk@semcog.org>
2023-03-16 09:44:26 -0400

    update json that drives the dropdown, update link and add extra slide helper text

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,7 @@ margin-top: 5%; margin-bottom:5%;">
           meeting.</p>
         <ul>
           <li><a href="https://semcog.org/desktopmodules/SEMCOG.Publications/GetFile.ashx?filename=2050%20Regional%20Forecast%20by%20County%20and%20Community.pdf" target="_blank">2050 Regional Forecast by County and Community</a></li>
-          <li>2050 Regional Forecast Presentation to the Executive Committee</li>
+          <li><a href="https://semcog.org/desktopmodules/SEMCOG.Publications/GetFile.ashx?filename=2050%20Forecast%20Exec%20Presentation.pdf" target="_blank">2050 Regional Forecast Presentation to the Executive Committee</a></li>
         </ul>
         <p>Prior to completing the draft 2050 Regional Forecast for Counties and Communities, SEMCOG held a series of
           Community Stakeholder open house meetings for the 2050 Regional Development Forecast. Three meetings were held
@@ -95,7 +95,7 @@ margin-top: 5%; margin-bottom:5%;">
         will be older than 65 and, the older population is projected to outnumber the children (under 18 years) for the
         first time in the region’s history. Because of these transformative trends, net international migration is
         expected to overtake natural increase as a leading cause of population growth in the coming decades.</p>
-      <p class="blog_margin">Use the map below to pan, zoom (using the +, or double click), or search to explore your community.</p>
+      <p class="blog_margin">Use the map below to pan, zoom (using the +, or double click), or search to explore your community. Moving your mouse over a slide carousel will pause the slides. You can right click to download a slide as an image.</p>
       <div class="insight_section" v-if="!printOnLoad">
         <hooper class="carousel_left" :settings="demographics_carousel">
           <slide >
@@ -120,7 +120,7 @@ margin-top: 5%; margin-bottom:5%;">
         transportation and warehousing, and construction will provide jobs for our residents. Employment in the
         manufacturing sector remains stable through 2030, but will start to decline after 2030 as the auto industry
         increasingly turns its attention to electric vehicle production.</p>
-      <p class="blog_margin">Use the map below to pan, zoom (using the +, or double click), or search to explore your community.</p>
+      <p class="blog_margin">Use the map below to pan, zoom (using the +, or double click), or search to explore your community. Moving your mouse over a slide carousel will pause the slides. You can right click to download a slide as an image.</p>
       <div class="insight_section" v-if="!printOnLoad">
         <hooper class="carousel_right" :settings="demographics_carousel">
           <slide>
@@ -154,7 +154,7 @@ margin-top: 5%; margin-bottom:5%;">
         expectations and collect data on planned development. Future residential growth is expected to be more evenly
         split between single-family and multi-family housing units. The region will add over 100 million square feet of
         new nonresidential buildings, concentrated in buildings that are fewer in number but larger in size.</p>
-      <p class="blog_margin">Use the map below to pan, zoom (using the +, or double click), or search to explore your community.</p>
+      <p class="blog_margin">Use the map below to pan, zoom (using the +, or double click), or search to explore your community. Moving your mouse over a slide carousel will pause the slides. You can right click to download a slide as an image.</p>
       <div class="insight_section" v-if="!printOnLoad">
         <hooper class="carousel_left" :settings="demographics_carousel">
           <slide>
