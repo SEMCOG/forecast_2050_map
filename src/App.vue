665c47c411c1d2cb635b5b52bf8f1575b62c8750
Chad Misiuk <misiuk@semcog.org>
2026-01-02 08:58:25 -0500

    update npm and fix a watch by using the reactiveUtils instead

--- src/App.vue
+++ src/App.vue
@@ -1834,11 +1834,15 @@ export default {
       }
     })
 
-    this.view.popup.watch("selectedFeature", (graphic) => {
-      if (graphic) {
-        this.selectedFeature = this.view.popup.selectedFeature.attributes
-      }
-    });
+    reactiveUtils.watch(
+        () => this.view.popup.selectedFeature,
+        (graphic) => {
+          if (graphic) {
+            this.selectedFeature = this.view.popup.selectedFeature.attributes
+          }
+        },
+    )
+
 
     this.view.whenLayerView(this.forecast_layer_info).then((layerView) => {
       layerView.filter = {
@@ -1939,12 +1943,15 @@ export default {
   },
   watch: {
     selectedFeature: function () {
-      let popup_geoids = this.view.popup.features.map((f) => {
-        return f.attributes.geoid
-      })
-      if (!popup_geoids.includes(this.selectedFeature.geoid)) {
-        this.view.popup.close()
+      if (this.view.popup.features && this.view.popup.features.length > 0) {
+        let popup_geoids = this.view.popup.features.map((f) => {
+          return f.attributes.geoid
+        })
+        if (!popup_geoids.includes(this.selectedFeature.geoid)) {
+          this.view.popup.close()
+        }
       }
+
       if (this.selectedFeature && parseInt(this.selectedFeature.geoid, 10) !== 8999) {
         this.view.whenLayerView(this.forecast_layer_info).then((layerView) => {
               let queryObject = new Query();
